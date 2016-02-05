b870756b6df1241f905b68a86c1db6ac080968a8
FRC-3277 <prodigi3277@gmail.com>
2016-02-04 20:03:20 -0600

    Camera was tested, it didn't work all too much. Will try more

---
+++ src/Subsystems/Camera.cpp
@@ -0,0 +1,18 @@
+#include "Camera.h"
+#include "../RobotMap.h"
+
+Camera::Camera() :
+		Subsystem("ExampleSubsystem")
+{
+
+}
+
+void Camera::InitDefaultCommand()
+{
+	// Set the default command for a subsystem here.
+	//SetDefaultCommand(new MySpecialCommand());
+
+}
+
+// Put methods for controlling this subsystem
+// here. Call these from Commands.
---
+++ src/Subsystems/Camera.h
@@ -0,0 +1,20 @@
+#ifndef Camera_H
+#define Camera_H
+
+#include "Commands/Subsystem.h"
+#include "WPILib.h"
+
+class Camera: public Subsystem
+{
+private:
+	// It's desirable that everything possible under private except
+	// for methods that implement subsystem capabilities
+	std::shared_ptr< USBCamera > bob_camera;
+
+
+public:
+	Camera();
+	void InitDefaultCommand();
+};
+
+#endif
